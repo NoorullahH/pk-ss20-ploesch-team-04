1655f98b1e9211581ed707df6d2b5754d66507f7
reinhold <reinhold.ploesch@jku.at>
2020-03-11 13:07:04 +0100

    Initial version

---
+++ src/test/java/at/jku/se/taskmgmt/TaskTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.jku.se.taskmgmt;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author reinhold
+ */
+public class TaskTest {
+    
+   private final Task t= new Task("MyTask");
+
+    /**
+     * Test of setTaskName method, of class Task.
+     */
+    @Test
+    void setTaskName() {
+        t.setTaskName("MyNewTask");
+        assertEquals(t.getTaskName(), "MyNewTask");
+    }
+    
+}
