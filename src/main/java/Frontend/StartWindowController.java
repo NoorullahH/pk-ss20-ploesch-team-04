a53a6a80de3a672d428fd093c3a0dade8dc4738d
NoorullahH <hamid.noorullah@yahoo.de>
2020-03-12 21:46:39 +0100

    FirstVersiontest
    @Noorullah

--- src/main/java/Frontend/StartWindowController.java
+++ src/main/java/Frontend/StartWindowController.java
@@ -14,18 +14,10 @@ public class StartWindowController {
 
 	@FXML
 	private void createNewTask(ActionEvent event) throws IOException {
-		
-    	//Node source = (Node) event.getSource();
-    	//Stage window = (Stage) source.getScene().getWindow();
-    	
+	
     	Parent parent = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
     	Scene scene = new Scene (parent);
     	Stage windowStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    	
-    	//FXMLLoader loader = new FXMLLoader(getClass().getResource("mainWindow.fxml"));
-		//Parent root = loader.load();
-    	//Scene hs = new Scene(root);
-    	
     	windowStage.setScene(scene);
     	windowStage.show();
     }
