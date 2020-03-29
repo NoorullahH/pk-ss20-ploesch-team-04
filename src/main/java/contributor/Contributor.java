9458f7269ea1195102dec17c2d0ee23d667a87da
Mara <Mara@10.0.0.8>
2020-03-29 23:42:20 +0200

    Mitwirkende erstellt

---
+++ src/main/java/contributor/Contributor.java
@@ -0,0 +1,19 @@
+package contributor;
+
+public class Contributor {
+	
+	private String person;
+	
+	public Contributor(String person) {
+		this.person = person;
+	}
+	
+	public String getPerson() {
+		return person;
+	}
+	
+	public void setPerson(String person) {
+		this.person = person;
+	}
+	
+}
