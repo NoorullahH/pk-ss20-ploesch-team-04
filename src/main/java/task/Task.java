510a844799a7492b66e91fbae58e048de086c7b1
Mara <Mara@10.0.0.8>
2020-03-29 23:49:08 +0200

    Tasks erstellen

--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -1,79 +1,174 @@
 package task;
 
 import java.time.LocalDate;
-import java.util.Comparator;
-import javax.xml.bind.annotation.XmlElement;
+import java.util.LinkedList;
 
-import javafx.beans.property.SimpleStringProperty;
+import category.Category;
+import contributor.Contributor;
+import weekday.Weekday;
+
 
 public class Task{
 
-	
 	private String taskDescription;
+	private String detailedTaskDescription;
+	private LocalDate dueDate;
+	private LinkedList<Contributor> contributors;
+	private boolean recurrent;
+	private boolean weekly;
+	private Weekday weekday;  
+	private boolean monthly;
+	private int monthday;
+	private int numberOfRepetitions;
+	private LinkedList<Category> categories;
+	private LinkedList<String> subtasks;
+	private LinkedList<String> attachments;
+	 	
 	
-	private String detailsDescription;
-	
-	private String contributors;
-	
-	private String category;
-	
-	//private String attachment;
-	
-	private LocalDate date;
-	
-	
-	public Task() {
-		
+	public Task(String taskDescription, String detailedTaskDescription,LocalDate dueDate, LinkedList<Contributor> contributors, boolean recurrent, boolean weekly, Weekday weekday, boolean monthly, int monthday, int numberOfRepetitions, LinkedList<Category> categories, LinkedList<String> subtasks, LinkedList<String> attachments) {
+		this.taskDescription = taskDescription;
+		this.detailedTaskDescription = detailedTaskDescription;
+		this.dueDate = dueDate;
+	 	this.contributors = contributors;
+		this.recurrent = recurrent;
+		this.weekly = weekly;
+		this.weekday = weekday;
+		this.monthly = monthly;
+		this.monthday = monthday;
+		this.numberOfRepetitions = numberOfRepetitions;
+		this.categories = categories;
+		this.subtasks = subtasks;
+		this.attachments = attachments;
 	}
 
-	public Task(String taskDescription, String detailsDescription, String contributors, String category, LocalDate date) {
-		super();
-		this.taskDescription = taskDescription;
-		this.detailsDescription = detailsDescription;
-		this.contributors = contributors;
-		this.category = category;
-		this.date = date;
-	}
 
 	public String getTaskDescription() {
 		return taskDescription;
 	}
 
+
 	public void setTaskDescription(String taskDescription) {
 		this.taskDescription = taskDescription;
 	}
 
-	public String getDetailsDescription() {
-		return detailsDescription;
+
+	public String getDetailedTaskDescription() {
+		return detailedTaskDescription;
 	}
 
-	public void setDetailsDescription(String detailsDescription) {
-		this.detailsDescription = detailsDescription;
+
+	public void setDetailedTaskDescription(String detailedTaskDescription) {
+		this.detailedTaskDescription = detailedTaskDescription;
 	}
 
-	public String getContributors() {
+
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+
+
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+
+
+	public LinkedList<Contributor> getContributors() {
 		return contributors;
 	}
 
-	public void setContributors(String contributors) {
+
+	public void setContributors(LinkedList<Contributor> contributors) {
 		this.contributors = contributors;
 	}
 
-	public String getCategory() {
-		return category;
-	}
 
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public LocalDate getDate() {
-		return date;
-	}
-
-	public void setDate(LocalDate date) {
-		this.date = date;
+	public boolean isRecurrent() {
+		return recurrent;
 	}
 
 
+	public void setRecurrent(boolean recurrent) {
+		this.recurrent = recurrent;
+	}
+
+
+	public boolean isWeekly() {
+		return weekly;
+	}
+
+
+	public void setWeekly(boolean weekly) {
+		this.weekly = weekly;
+	}
+
+
+	public Weekday getWeekday() {
+		return weekday;
+	}
+
+
+	public void setWeekday(Weekday weekday) {
+		this.weekday = weekday;
+	}
+
+
+	public boolean isMonthly() {
+		return monthly;
+	}
+
+
+	public void setMonthly(boolean monthly) {
+		this.monthly = monthly;
+	}
+
+
+	public int getMonthday() {
+		return monthday;
+	}
+
+
+	public void setMonthday(int monthday) {
+		this.monthday = monthday;
+	}
+
+
+	public int getNumberOfRepetitions() {
+		return numberOfRepetitions;
+	}
+
+
+	public void setNumberOfRepetitions(int numberOfRepetitions) {
+		this.numberOfRepetitions = numberOfRepetitions;
+	}
+
+
+	public LinkedList<Category> getCategories() {
+		return categories;
+	}
+
+
+	public void setCategories(LinkedList<Category> categories) {
+		this.categories = categories;
+	}
+
+
+	public LinkedList<String> getSubtasks() {
+		return subtasks;
+	}
+
+
+	public void setSubtasks(LinkedList<String> subtasks) {
+		this.subtasks = subtasks;
+	}
+
+
+	public LinkedList<String> getAttachments() {
+		return attachments;
+	}
+
+
+	public void setAttachments(LinkedList<String> attachments) {
+		this.attachments = attachments;
+	}
+	 
 }
