f788a04fe37ea72f099eefbfc178b70bd6f29d07
Mara <Mara@10.0.0.8>
2020-03-31 14:26:03 +0200

    Klasse Subtasks erstellt

---
+++ src/main/java/task/Subtask.java
@@ -0,0 +1,19 @@
+package task;
+
+public class Subtask {
+	
+	private String subtask;
+	
+	public Subtask(String subtask) {
+		this.subtask = subtask;
+	}
+
+	public String getSubtask() {
+		return subtask;
+	}
+
+	public void setSubtask(String subtask) {
+		this.subtask = subtask;
+	}
+
+}
